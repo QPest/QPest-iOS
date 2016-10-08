16bc4d8ae22bd24790be5d992cb181ea70bb2c23
Henrique <hlopes13@hotmail.com>
2016-10-08 10:39:57 -0300

    Getting user location

---
+++ QPest/ColorPalette.swift
@@ -0,0 +1,19 @@
+//
+//  ColorPalette.swift
+//  Cartola VIP
+//
+//  Created by Henrique Dutra on 14/07/16.
+//  Copyright © 2016 Henrique Dutra. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColorPalette: NSObject {
+    
+    static let defaultPalette = ColorPalette()
+    
+    let pragueTableViewColor = UIColor.colorWithHexString(hex: "E3625F")
+    let naturalEnemyTableViewColor = UIColor.colorWithHexString(hex: "B8E986")
+    
+}
--- QPest/MapMainViewController.swift
+++ QPest/MapMainViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapMainViewController: UIViewController {
+class MapMainViewController: UIViewController, CLLocationManagerDelegate{
 
     let mapTitle = "Meu Terreno"
+    let locationManager = CLLocationManager()
+    var locationCoordinate = CLLocation(latitude: 37.785834, longitude: -122.406417)
+    let regionRadius: CLLocationDistance = 1000
+
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -19,8 +24,11 @@ class MapMainViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        
+        self.setuplocation()
         self.setupNavigationBar()
+    
+        //self.centerMapOnLocation(location: self.locationCoordinate)
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -33,4 +41,28 @@ class MapMainViewController: UIViewController {
         self.navigationItem.title = self.mapTitle
     }
 
+    private func setuplocation(){
+    
+        locationManager.requestAlwaysAuthorization()
+
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+  
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+//        self.locationCoordinate.coordinate.latitude = locValue.latitude
+        
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }

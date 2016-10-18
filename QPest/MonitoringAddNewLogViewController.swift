24117289a41241267c4f1914f669958b258eb3de
Henrique <hlopes13@hotmail.com>
2016-10-18 10:56:36 -0200

    Text field framework added, architecture for adding manual log added, visual modifications

--- QPest/MonitoringAddNewLogViewController.swift
+++ QPest/MonitoringAddNewLogViewController.swift
@@ -7,27 +7,84 @@
 //
 
 import UIKit
+import TextFieldEffects
 
-class MonitoringAddNewLogViewController: UIViewController {
-
+class MonitoringAddNewLogViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var quantityTextField: HoshiTextField!
+    @IBOutlet weak var pragueTextField: HoshiTextField!
+    
+    var quantity : Int = Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.setupNavigationBar()
+        self.hideKeyboardWhenTappedAround()
         
+        self.quantityTextField.delegate = self
+        self.pragueTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-  
 
-    private func setupNavigationBar(){
+    @IBAction func didClickConfirmation(_ sender: AnyObject) {
     
-        self.navigationItem.title = ""
+        self.checkFields()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    private func checkFields(){
+
+        var verification : Bool = false
         
+        if let _ = Int(self.quantityTextField.text!) {
+            if (self.pragueTextField.text != ""){
+                verification = true
+            }
+        }
+        
+        self.fieldAction(value: verification)
+
+    }
+    
+    private func fieldAction(value : Bool){
+    
+        if value {
+            self.createNewLog()
+        }
+        else{
+            self.putMessage()
+        }
+    }
+    
+    private func putMessage(){
+        
+        //
     }
 
+    private func createNewLog(){
+    
+        let newLog = MonitoringLog()
+
+        newLog.date = Date()
+        newLog.pragueName = self.pragueTextField.text!
+        newLog.pragueQuantity = self.quantity
+        
+        self.dismissView()
+    }
+    
+    func dismissView(){
+    
+       _ = self.navigationController?.popViewController(animated: true)
+
+    }
+    
     
 }
---
+++ QPest/UIViewController+Util.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
